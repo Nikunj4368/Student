97ae6af8dbf34145b3fd0c1f6c8e8330eb2547a0
Nikunj4368 <69626678+Nikunj4368@users.noreply.github.com>
2020-09-24 17:05:23 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,4 +1,3 @@
 <?php
-$con=mysqli_connect('student.cvjrpevsl4qy.us-east-1.rds.amazonaws.com','student','Nirma.4368','student') or die("CONNECTION UNSUCCESSFULL...!");
-
+$con=mysqli_connect('student.cvjrpevsl4qy.us-east-1.rds.amazonaws.com','admin','Nirma.4368','student','3306') or die("CONNECTION UNSUCCESSFULL...!");
 ?>
